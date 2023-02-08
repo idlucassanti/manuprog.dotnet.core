b8b9c0e85b54da9f1dbd7a26d5fb026be0f5b138
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-08 20:50:56 -0300

    Criação da Entidade Pessoa

---
+++ src/MP.Core.Domain/Entities/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using MP.Core.Domain.Validations;
+
+namespace MP.Core.Domain.Entities
+{
+    public sealed class Pessoa
+    {
+        public Pessoa(string nome, string documento, string telefone)
+        {
+            Validation(nome, documento, telefone);
+        }
+
+        public Pessoa(int id, string nome, string documento, string telefone)
+        {
+            DomainValidationException.When(id < 0, "Id deve ser maior que zero!");
+            Id = id;
+
+            Validation(nome, documento, telefone);
+        }
+
+        public int Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Documento { get; private set; }
+
+        public string Telefone { get; private set; }
+
+        private void Validation(string nome, string documento, string telefone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(nome), "Nome deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(documento), "Documento deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(telefone), "Telefone deve ser informado!");
+
+            Nome = nome;
+            Documento = documento;
+            Telefone = telefone;
+        }
+    }
+}
---
+++ src/MP.Core.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,17 @@
+﻿namespace MP.Core.Domain.Validations
+{
+    internal class DomainValidationException : Exception
+    {
+        public DomainValidationException(string erro)
+            :base(erro)
+        { }
+
+        public static void When(bool hasError, string message)
+        {
+            if(hasError)
+            {
+                throw new DomainValidationException(message);
+            }
+        }
+    }
+}
