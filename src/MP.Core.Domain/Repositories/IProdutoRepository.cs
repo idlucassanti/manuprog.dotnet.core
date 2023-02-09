ee7da0d831f49602ce5478172e381702a95f9a38
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-09 11:14:52 -0300

    Criando Contrato e o  Repositório da Entidade Produto

---
+++ src/MP.Core.Domain/Repositories/IProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using MP.Core.Domain.Entities;
+
+namespace MP.Core.Domain.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task<Produto> ObterProdutoPorIdAsync(int produtoId);
+
+        Task<ICollection<Produto>> ObterListaProdutosAsync();
+
+        Task<Produto> InserirProduto(Produto produto);
+
+        Task AtualizarProduto(Produto produto);
+
+        Task DeletarProduto(Produto produto);
+    }
+}
---
+++ src/MP.Core.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MP.Core.Domain.Entities;
+using MP.Core.Domain.Repositories;
+using MP.Core.Infra.Data.Context;
+
+namespace MP.Core.Infra.Data.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly MpDbContext _connection;
+
+        public ProdutoRepository(MpDbContext context)
+        {
+            _connection = context;
+        }
+
+        public async Task<ICollection<Produto>> ObterListaProdutosAsync()
+        {
+            return await _connection.Produtos.ToListAsync();
+        }
+
+        public async Task<Produto> ObterProdutoPorIdAsync(int produtoId)
+        {
+            return await _connection.Produtos.FirstOrDefaultAsync(p => p.Id.Equals(produtoId));
+        }
+
+        public async Task<Produto> InserirProduto(Produto produto)
+        {
+            _connection.Produtos.Add(produto);
+            await _connection.SaveChangesAsync();
+
+            return produto;
+        }
+
+        public async Task AtualizarProduto(Produto produto)
+        {
+            _connection.Produtos.Update(produto);
+            await _connection.SaveChangesAsync();
+        }
+
+        public async Task DeletarProduto(Produto produto)
+        {
+            _connection.Produtos.Remove(produto);
+            await _connection.SaveChangesAsync();
+        }
+    }
+}
