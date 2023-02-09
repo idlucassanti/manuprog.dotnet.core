aafe477ab8e8f80a9e3ddeeda85856e0ec82581e
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-08 21:03:52 -0300

    Ciração da Entidade Produto

---
+++ src/MP.Core.Domain/Entities/Produto.cs
@@ -0,0 +1,37 @@
+﻿using MP.Core.Domain.Validations;
+
+namespace MP.Core.Domain.Entities
+{
+    public class Produto
+    {
+        public Produto(string nome, string codigo, decimal preco)
+        {
+            Validation(nome, codigo, preco);
+        }
+
+        public Produto(int id, string nome, string codigo, decimal preco)
+        {
+            DomainValidationException.When(id <= 0, "Id deve ser maior que zero");
+            Validation(nome, codigo, preco);
+        }
+
+        public int Id { get; set; }
+
+        public string Nome { get; private set; }
+
+        public string Codigo { get; private set; }
+
+        public decimal Preco { get; private set; }
+
+        private void Validation(string nome, string codigo, decimal preco)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(nome), "Nome deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(codigo), "Código deve ser informado!");
+            DomainValidationException.When(preco < 0, "Preço deve ser maior que zero!");
+
+            Nome = nome;
+            Codigo = codigo;
+            Preco = preco;
+        }
+    }
+}
