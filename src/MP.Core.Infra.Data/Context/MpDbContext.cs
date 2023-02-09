bee7b9b354b3ee286122f95c054293374ffe9904
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-08 22:53:39 -0300

    Criação da Conexão com Banco de Dados e Instalação dos pacotes PostgreSQL

---
+++ src/MP.Core.Infra.Data/Context/MpDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MP.Core.Domain.Entities;
+
+namespace MP.Core.Infra.Data.Context
+{
+    public class MpDbContext : DbContext
+    {
+        public MpDbContext(DbContextOptions<MpDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MpDbContext).Assembly);
+        }
+    }
+}
