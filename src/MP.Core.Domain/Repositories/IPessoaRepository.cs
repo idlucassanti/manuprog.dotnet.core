30e3ce2ea67f5970b3197198d90ae4561c44a54d
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-09 11:14:42 -0300

    Criando Contrato e o  Repositório da Entidade Pessoa

---
+++ src/MP.Core.Domain/Repositories/IPessoaRepository.cs
@@ -0,0 +1,17 @@
+﻿using MP.Core.Domain.Entities;
+
+namespace MP.Core.Domain.Repositories
+{
+    public interface IPessoaRepository
+    {
+        Task<Pessoa> ObterPessoaPorIdAsync(int pessoaId);
+
+        Task<ICollection<Pessoa>> ObterListaPessoasAsync();
+
+        Task<Pessoa> InserirPessoaAsync(Pessoa pessoa);
+
+        Task AtualizarPessoaAsync(Pessoa pessoa);
+
+        Task DeletarPessoaAsync(Pessoa pessoa);
+    }
+}
---
+++ src/MP.Core.Infra.Data/Repositories/PessoaRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MP.Core.Domain.Entities;
+using MP.Core.Domain.Repositories;
+using MP.Core.Infra.Data.Context;
+
+namespace MP.Core.Infra.Data.Repositories
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        private readonly MpDbContext _connection;
+
+        public PessoaRepository(MpDbContext context)
+        {
+            _connection = context;
+        }
+
+        public async Task<ICollection<Pessoa>> ObterListaPessoasAsync()
+        {
+            return await _connection.Pessoas.ToListAsync();
+        }
+
+        public async Task<Pessoa> ObterPessoaPorIdAsync(int pessoaId)
+        {
+            return await _connection.Pessoas.FirstOrDefaultAsync(p => p.Id.Equals(pessoaId));
+        }
+
+        public async Task<Pessoa> InserirPessoaAsync(Pessoa pessoa)
+        {
+            _connection.Pessoas.Add(pessoa);
+            await _connection.SaveChangesAsync();
+            return pessoa;
+        }
+
+        public async Task AtualizarPessoaAsync(Pessoa pessoa)
+        {
+            _connection.Pessoas.Update(pessoa);
+            await _connection.SaveChangesAsync();
+        }
+
+        public async Task DeletarPessoaAsync(Pessoa pessoa)
+        {
+            _connection.Pessoas.Remove(pessoa);
+            await _connection.SaveChangesAsync();
+        }
+    }
+}
