9fe2ff9f7082ad28adf86a60816528c29e9faf0e
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-10 17:46:32 -0300

    Criação DTO Pessoa e Validator(FluentValidation)

---
+++ src/MP.Core.Application/DTOs/PessoaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace MP.Core.Application.DTOs
+{
+    public class PessoaDTO
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Documento { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
---
+++ src/MP.Core.Application/Validators/PessoaValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MP.Core.Application.DTOs;
+
+namespace MP.Core.Application.Validators
+{
+    public class PessoaValidator : AbstractValidator<PessoaDTO>
+    {
+        public PessoaValidator() 
+        { 
+            RuleFor(x => x.Nome)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Nome deve ser informado.");
+
+            RuleFor(x => x.Documento)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Documento deve ser informado.");
+
+            RuleFor(x => x.Telefone)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Telefone deve ser informado.");
+
+        }
+    }
+}
