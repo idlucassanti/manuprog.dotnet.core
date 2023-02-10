464a0c2961dcfbb9105b9c8f4212116747721081
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-10 20:45:49 -0300

    Criação do Controller Pessoas com Action InserirAsync

---
+++ src/MP.Core.Api/Controllers/PessoasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MP.Core.Application.DTOs;
+using MP.Core.Application.Services.Interfaces;
+
+namespace MP.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoasController : ControllerBase
+    {
+        private readonly IPessoaService _pessoaService;
+
+        public PessoasController(IPessoaService pessoaService)
+        {
+            _pessoaService = pessoaService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InserirAsync([FromBody] PessoaDTO pessoaDTO)
+        {
+            var result = await _pessoaService.InserirAsync(pessoaDTO);
+
+            if (result.IsSuccess) return Ok(result);
+
+            return BadRequest(result);
+        }
+    }
+}
