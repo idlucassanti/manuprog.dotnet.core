1d94072f3eb9b50271c07b4e3f91e86e416d023d
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-10 17:48:37 -0300

    Criação Services Genérico tratando todos os Retornos

---
+++ src/MP.Core.Application/Services/ResultService.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation.Results;
+
+namespace MP.Core.Application.Services
+{
+    public class ResultService
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public ICollection<ErrorValidation> Errors { get; set; }
+
+        public static ResultService RequestError(string message, ValidationResult validationResult)
+        {
+            var result = new ResultService()
+            {
+                IsSuccess = false,
+                Message = message,
+                Errors = validationResult.Errors.Select(x => new ErrorValidation { Field = x.PropertyName, Message = x.ErrorMessage }).ToList(),
+            };
+
+            return result;
+        }
+
+        public static ResultService<T> RequestError<T>(string message, ValidationResult validationResult)
+        {
+            var result = new ResultService<T>()
+            {
+                IsSuccess = false,
+                Message = message,
+                Errors = validationResult.Errors.Select(x => new ErrorValidation { Field = x.PropertyName, Message = x.ErrorMessage }).ToList()
+            };
+
+            return result;
+        }
+
+        public static ResultService Fail(string message)
+        {
+            var result = new ResultService()
+            {
+                IsSuccess = false,
+                Message = message
+            };
+
+            return result;
+        }
+
+        public static ResultService<T> Fail<T>(string message)
+        {
+            var result = new ResultService<T>()
+            {
+                IsSuccess = false,
+                Message = message
+            };
+
+            return result;
+        }
+
+        public static ResultService Ok(string message)
+        {
+            var result = new ResultService()
+            {
+                IsSuccess = true,
+                Message = message
+            };
+
+            return result;
+        }
+
+        public static ResultService<T> Ok<T>(T data)
+        {
+            var result = new ResultService<T>()
+            {
+                IsSuccess = true,
+                Data = data
+            };
+
+            return result;
+        }
+    }
+
+    public class ResultService<T> : ResultService
+    { 
+        public T Data { get; set; }
+    }
+}
