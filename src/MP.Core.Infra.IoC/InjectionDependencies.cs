70cb5df2f023f989ca2cbb649ad6b92f9c0adc8e
Lucas Santi <lsanti.3mpk@sofisa.com.br>
2023-02-10 19:35:11 -0300

    Criação da Injeção de Dependências

---
+++ src/MP.Core.Infra.IoC/InjectionDependencies.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MP.Core.Application.Mappings;
+using MP.Core.Application.Services;
+using MP.Core.Application.Services.Interfaces;
+using MP.Core.Domain.Repositories;
+using MP.Core.Infra.Data.Context;
+using MP.Core.Infra.Data.Repositories;
+
+namespace MP.Core.Infra.IoC
+{
+    public static class InjectionDependencies
+    {
+        public static IServiceCollection AddIoC(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MpDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddAutoMapper(typeof(DomainToDtoMapping));
+
+            services.AddScoped<IPessoaService, PessoaService>();
+            
+            services.AddScoped<IPessoaRepository, PessoaRepository>();
+
+            return services;
+        }
+    }
+}
